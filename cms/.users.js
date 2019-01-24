9b9c08db06f7b4d5367182b2bf5494115a5863a6
Eeo Jun <141bytes@gmail.com>
2019-01-24 18:29:34 +0000

    initial commit

---
+++ cms/.users.js
@@ -0,0 +1,11 @@
+({
+	users: [
+		{
+			username: 'admin',
+			tags: [],
+			salt: 'a2fda0e883d5d41d25289151dee6c9dd',
+			hash: 'a803691b5a54350d00cc9e5f420325a286a9e2f37a0795e39054159735a1e0b7',
+			user_created_timestamp: 1548353469029
+		}
+	]
+})
