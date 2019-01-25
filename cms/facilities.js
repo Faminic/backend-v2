1be3523d2b1390cfca9ec9aeb7db0c1940a1c7ae
cornwallluke <luke@cornwall-jones.com>
2019-01-25 22:10:03 +0000

    I have added facility page adding and a basic facility page (no automisation)

---
+++ cms/facilities.js
@@ -0,0 +1,15 @@
+({
+	Banner: '/remote/direct_uploads/1548453958_Banner-IsabelleSmash.jpg',
+	$Banner_type: 'image',
+	Header: 'Our Facilities',
+	Description: 'These are all of our facilities here at pvcc.',
+	$Facilities_templatitator: '@@global.facility_card',
+	Facilities: [
+		{
+			Relative_Link: '/facilities/facility2',
+			Image: '/remote/direct_uploads/1548453697_Capture2.PNG',
+			$Image_type: 'Image',
+			Facility_Name: 'Example Facility Page'
+		}
+	]
+})
