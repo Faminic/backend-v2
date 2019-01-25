841a3e0e8afdff51417b8f962a31321d7100cc04
cornwallluke <luke@cornwall-jones.com>
2019-01-25 19:42:36 +0000

    pls gabe

--- cms/global/global.js
+++ cms/global/global.js
@@ -1,3 +1,4 @@
 {
     title: "Park View Community Centre",
+	
 }
---
+++ cms/global/templates/Pictures.js
@@ -0,0 +1,12 @@
+{
+    $Picture_hidden: true,
+    Picture: {
+        Image: {
+            $Image_type: 'image',
+            Image: '',
+			
+            Name: '',
+        },
+    },
+    $Picture_templatitator: '@@Picture',
+}
