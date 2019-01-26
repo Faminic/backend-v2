5cb0ba5965e13f4abc1945979322e07c0aa45734
cornwallluke <luke@cornwall-jones.com>
2019-01-26 13:39:51 +0000

    all facilities pages added and mainpage created

---
+++ cms/generators/Facilities/Facilities.js
@@ -0,0 +1,22 @@
+({
+	Name: '',
+	$Description_type:"quill",
+	Description: '',
+	Banner: '',
+	$Banner_type: 'image',
+	Thumbnail: '',
+	$Thumbnail_type: 'image',
+	$Facility_Images_templatitator: '@@global.Picture',
+	Facility_Images: [
+	],
+	Available_Times: {
+		Monday: '',
+		Tuesday: '',
+		Wednesday: '',
+		Thursday: '',
+		Friday: '',
+		Saturday: '',
+		Sunday: ''
+	},
+	$Description_control_path: '/brick/enduro_quill/quill_control'
+})
---
+++ cms/generators/Facilities/theatre.js
@@ -0,0 +1,37 @@
+({
+	Name: 'Theatre',
+	$Description_type: 'quill',
+	Description: '<p><span style="background-color: rgb(255, 255, 255); color: rgb(102, 102, 102);">The recently re-decorated, 250 seated theatre has a traditional proscenium arch and stage and thus is a professional setting for performances or presentations of any type. Because the seating folds back, the space is sufficiently flexible to accommodate theatre in the round, more intimate musical events or fitness-related and dance activities. It is heated and well lit, with disabled access catered for – a beautiful setting for any event.</span></p><p><br></p><p><span style="background-color: rgb(255, 255, 255); color: rgb(102, 102, 102);">Click here to see a 4D virtual tour of our theatre.</span></p>',
+	Banner: '/remote/direct_uploads/1548507485_image.png',
+	$Banner_type: 'image',
+	Thumbnail: '/remote/direct_uploads/1548507486_image.png',
+	$Thumbnail_type: 'image',
+	$Facility_Images_templatitator: '@@global.Picture',
+	Facility_Images: [
+		{
+			$Image_type: 'image',
+			Image: '/remote/direct_uploads/1548507501_image.png',
+			Caption: ''
+		},
+		{
+			$Image_type: 'image',
+			Image: '/remote/direct_uploads/1548507489_image.png',
+			Caption: ''
+		},
+		{
+			$Image_type: 'image',
+			Image: '/remote/direct_uploads/1548507495_image.png',
+			Caption: ''
+		}
+	],
+	Available_Times: {
+		Monday: '6:00 pm – 9:30 pm',
+		Tuesday: '6:00 pm – 9:30 pm',
+		Wednesday: '6:00 pm – 9:30 pm',
+		Thursday: '6:00 pm – 9:30 pm',
+		Friday: '6:00 pm – 9:30 pm',
+		Saturday: '10:00 am – 4:30 pm',
+		Sunday: '10:00 am – 12:30 pm'
+	},
+	$Description_control_path: '/brick/enduro_quill/quill_control'
+})
