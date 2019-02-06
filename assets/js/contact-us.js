5db4938365a20f303cefe8d0f4b520523a40eff4
Faisal <faisalamin1999@googlemail.com>
2019-02-06 22:13:00 +0000

    contact-us js fix

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,15 +1,11 @@
-$(document).ready(function(){
+$("#contact-us-form").on("submit", function(e) {
+  e.preventDefault();
+  var name = $("#name").val();
+  var email = $("#email").val();
+  var phone = $("#phone").val();
+  var query = $("#query").val();
+  var test = {name: name, email: email, telephone: phone, query: query};
+  $.post("/api/contact-us", {name: name, email: email, telephone: phone, query: query}, function(data) {
 
-  $("#contact-us-form").on("submit", function(e) {
-    e.preventDefault();
-    var name = $("#itemNameEdit").val();
-    var email = $("#itemQuantityEdit").val();
-    var phone = $("#itemStorageEdit").val();
-    var query = $("#itemBuyEdit").val();
-    var test = {name: name, email: email, telephone: phone, query: query};
-    $.post("/api/contact-us", {name: name, email: email, telephone: phone, query: query}, function(data) {
-
-    })
   })
-
-}
+})
